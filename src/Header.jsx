e7fc6c76e180892cfa5c3a2151263cf279a344cd
Ahmed <ml967799@gmail.com>
2024-07-03 03:29:12 +0100

    Structuring main

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+//import 'Header.css' from './styles/header.css'; 
+export default function Header(){
+  return (
+    <div className="header">
+      <p>CV builder</p>
+      <a href="https://www.google.com">
+        a logo for the webApp
+      </a>
+    </div>
+  )
+}
---
+++ src/MainSection.jsx
@@ -0,0 +1,7 @@
+export default function MainSection() {
+  return (
+    <div className="main">
+      i'm main section
+    </div>
+  )
+}
